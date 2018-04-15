38ba439f4df0d8110897018329b904896cb23fd5
Benfarhat <benfarhat.elyes@gmail.com>
2018-04-15 19:31:40 +0100

    add class modifier

--- flexbox.js
+++ flexbox.js
@@ -31,3 +31,25 @@ for(let i = 0; i < closures.length; i++){
         event.defaultPrevented;
     }); 
 }
+
+/* Add / Remove Class
+You can for example change order propertie of an item */
+
+var modifiers = document.querySelectorAll('.modifier')
+for(let i = 0; i < modifiers.length; i++){
+   modifiers[i].addEventListener('click', function(event) {
+        let target = false || this.getAttribute("data-target")
+        let className = false || this.getAttribute("data-target-class")
+
+        if(target && className){ 
+            let element = false || document.querySelector(target)
+            if (element.classList.contains(className)){
+                element.classList.remove(className);
+            } else {
+                element.classList.add(className);
+            }
+        }
+
+        event.defaultPrevented;
+    }); 
+}
