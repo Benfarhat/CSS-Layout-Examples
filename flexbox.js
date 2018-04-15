cefb57dd5dd03e556ec3dc622462df882b7b94ed
Benfarhat <benfarhat.elyes@gmail.com>
2018-04-15 16:22:59 +0100

    Handle querySelector error

--- flexbox.js
+++ flexbox.js
@@ -4,16 +4,28 @@ Just add class="closuere" and a data-target that point to the element(s) you wan
 var closures = document.querySelectorAll('.closure')
 for(let i = 0; i < closures.length; i++){
    closures[i].addEventListener('click', function(event) {
-        let target = this.getAttribute("data-target")
-        let expand = this.getAttribute("data-expand")
-        let className = this.getAttribute("data-expand-class")
+        let target = false || this.getAttribute("data-target")
+        let expand = false || this.getAttribute("data-expand")
+        let className = false || this.getAttribute("data-expand-class")
 
-        if (document.querySelector(target).style.display == "none"){
-            document.querySelector(target).style.display = "block";
-            document.querySelector(expand).classList.remove(className);
-        } else {
-            document.querySelector(target).style.display = "none";
-            document.querySelector(expand).classList.add(className);
+        if(target){   
+            if (document.querySelector(target).style.display == "none"){
+                document.querySelector(target).style.display = "block";
+                if(expand && className){
+                    let element = false || document.querySelector(expand)
+                    if(element){
+                        element.classList.remove(className);
+                    }
+                }
+            } else {
+                document.querySelector(target).style.display = "none";
+                if(expand && className){
+                    let element =  false || document.querySelector(expand)
+                    if(element){
+                        element.classList.add(className);
+                    }
+                }
+            }
         }
 
         event.defaultPrevented;
